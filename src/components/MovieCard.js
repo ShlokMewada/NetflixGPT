e265991d044d7c126fcaa0344b499ea3715d869b
ShlokMewada <shlokmewada@gmail.com>
2024-07-30 13:56:34 +0530

    removing height from moviecards

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -3,7 +3,7 @@ import { IMG_CDN_URL } from "../utils/constants";
 const MovieCard = ({ poster_path }) => {
   return (
     <div className="w-44">
-      <img height="100px" src={IMG_CDN_URL + poster_path} alt="Movie card" />
+      <img src={IMG_CDN_URL + poster_path} alt="Movie card" />
     </div>
   );
 };
