c88e90eb589204b4377e2a519f2d2b6e7003c47c
ShlokMewada <shlokmewada@gmail.com>
2024-07-30 14:09:08 +0530

    fixing overview length in videotitle

--- src/components/VideoTitle.js
+++ src/components/VideoTitle.js
@@ -2,7 +2,7 @@ const VideoTitle = ({ title, overview }) => {
   return (
     <div className="absolute w-screen aspect-video flex flex-col pt-[20%] px-16 gap-y-3 bg-gradient-to-r from-black">
       <h1 className="text-6xl font-bold text-white">{title}</h1>
-      <p className="text-3xl w-1/4 text-white">{overview}</p>
+      {overview.length > 100 ? <p className="text-3xl w-1/4 text-white">{overview.substring(0,100)+"..."}</p> : <p>{overview}</p>}
       <div className="flex gap-x-3">
         <button className="flex justify-between items-baseline gap-x-5 rounded-md bg-white py-4 px-12 hover:bg-opacity-70">
           <i class="fa-solid fa-play"></i>
