5ce241512caec3e576d3dbb1973f93de4eda587a
ShlokMewada <shlokmewada@gmail.com>
2024-07-31 14:41:38 +0530

    responsive : videotitle change 1

--- src/components/VideoTitle.js
+++ src/components/VideoTitle.js
@@ -1,20 +1,22 @@
 const VideoTitle = ({ title, overview }) => {
   return (
     <div className="absolute overflow-x-hidden w-screen aspect-video flex flex-col pt-[20%] px-16 gap-y-3 bg-gradient-to-r from-black">
-      <h1 className="text-6xl font-bold text-white">{title}</h1>
+      <h1 className="text-lg md:text-6xl font-semibold md:font-bold text-white">
+        {title}
+      </h1>
       {overview.length > 100 ? (
-        <p className="text-3xl w-1/4 text-white">
+        <p className="hidden md:block text-3xl w-1/4 text-white">
           {overview.substring(0, 100) + "..."}
         </p>
       ) : (
-        <p>{overview}</p>
+        <p className="hidden md:block text-3xl w-1/4 text-white">{overview}</p>
       )}
       <div className="flex gap-x-3">
-        <button className="flex justify-between items-baseline gap-x-5 rounded-md bg-white py-4 px-12 hover:bg-opacity-70">
+        <button className="flex py-1 px-2 justify-between items-center md:items-baseline gap-x-5 md:gap-x-2 rounded-md bg-white md:py-4 md:px-12 hover:bg-opacity-70">
           <i className="fa-solid fa-play"></i>
           <p className="text-lg font-semibold">Play</p>
         </button>
-        <button className="flex justify-between items-baseline gap-x-5 rounded-md bg-gray-500 py-4 px-12 text-white bg-opacity-50 hover:bg-opacity-100">
+        <button className="flex py-1 px-2 items-center justify-between md:items-baseline gap-x-5 md:gap-x-2 rounded-md bg-gray-500 md:py-4 md:px-12 text-white bg-opacity-50 hover:bg-opacity-100">
           <i className="fa-solid fa-circle-info"></i>
           <p className="text-lg font-semibold">More Info</p>
         </button>
