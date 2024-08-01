c786b66e7d56d18e366cc4adf7ab760c6038f8e7
ShlokMewada <shlokmewada@gmail.com>
2024-08-01 15:19:58 +0530

    fixing slider issues and adding footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <div className="bg-gray-800 flex">
+      <div></div>
+    </div>
+  )
+}
+
+export default Footer
--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,53 +1,56 @@
+import React from "react";
 import MovieCard from "./MovieCard";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 const MovieList = ({ title, movies }) => {
-  const settings = {
-    dots: false,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 7,
-    slidesToScroll: 2,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
-          dots: true,
-        },
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          initialSlide: 2,
-        },
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        },
-      },
-    ],
-  };
-
-  // to remove empty spaces from carousal
   const movieResultsFinal = movies.filter(
     (movie) => movie.poster_path !== null
   );
+
+  const getSliderSettings = () => {
+    const length = movieResultsFinal.length;
+    return {
+      dots: false,
+      infinite: length > 1,
+      speed: 500,
+      slidesToShow: Math.min(7, length),
+      slidesToScroll: Math.min(2, length),
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: Math.min(3, length),
+            slidesToScroll: Math.min(3, length),
+            infinite: length > 3,
+            dots: length > 3,
+          },
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: Math.min(2, length),
+            slidesToScroll: Math.min(2, length),
+            initialSlide: Math.min(2, length - 1),
+          },
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+          },
+        },
+      ],
+    };
+  };
 
   return (
     <div className="flex flex-col gap-y-5 mt-6 px-7">
       <h1 className="text-3xl font-bold text-white">{title}</h1>
       <div className="">
-        <Slider {...settings}>
+        <Slider {...getSliderSettings()}>
           {movieResultsFinal.map((movie) => (
             <MovieCard key={movie.id} poster_path={movie.poster_path} />
           ))}
